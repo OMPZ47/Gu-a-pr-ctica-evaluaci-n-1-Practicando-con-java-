5ae340df1ecc446bd8b6796b8dc88dc284651bee
OMPZ47 <84553006+OMPZ47@users.noreply.github.com>
2026-08-19 21:53:38 -0600

    Update Ejercicio3.java

--- ejercicio3/Ejercicio3.java
+++ ejercicio3/Ejercicio3.java
@@ -1,6 +1,16 @@
-
 package ejercicio3;
-
+/*
+    Cree un programa que solicite un rango de años e imprima en 
+pantalla los años que son bisiestos dentro de ese rango establecido. 
+¿Cómo saber si un año es bisiesto? 
+➢ Todos los años bisiestos son divisibles entre 4. 
+➢ Aquellos años que son divisibles entre 4, pero no entre 100, son 
+bisiestos. 
+➢ Los años que son divisibles entre 100, pero no entre 400, no son 
+bisiestos. 
+➢ Sin embargo, los años divisibles entre 100 y entre 400 sí que son 
+bisiestos.
+*/
 
 public class Ejercicio3 {
 
